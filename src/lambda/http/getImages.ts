b53dfbe0bb715489c94f01fdb0fc839d62107739
Nicholas Pretorius <nicholaspretorius@gmail.com>
2020-03-18 15:44:58 +0200

    feat: getImages per group

---
+++ src/lambda/http/getImages.ts
@@ -0,0 +1,71 @@
+import { APIGatewayProxyHandler, APIGatewayProxyEvent, APIGatewayProxyResult } from 'aws-lambda';
+import "source-map-support/register";
+import * as AWS from "aws-sdk";
+
+const docClient = new AWS.DynamoDB.DocumentClient();
+
+const groupsTable = process.env.GROUPS_TABLE;
+const imagesTable = process.env.IMAGES_TABLE;
+
+export const handler: APIGatewayProxyHandler = async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
+
+    console.log("Processing Event: ", event);
+    const groupId = event.pathParameters.groupId
+
+    const validGroupId = groupExists(groupId);
+
+    if (!validGroupId) {
+        return {
+            statusCode: 404,
+            headers: {
+                "Access-Control-Allow-Origin": "*",
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify({
+                message: "Group does not exist"
+            })
+        }
+    }
+
+    const images = await getImagesPerGroup(groupId);
+
+    return {
+        statusCode: 200,
+        headers: {
+            "Access-Control-Allow-Origin": "*",
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify({
+            items: images
+        })
+    }
+}
+
+async function groupExists(groupId: string) {
+
+    const result = await docClient.get(
+        {
+            TableName: groupsTable,
+            Key: {
+                id: groupId
+            }
+        }
+    ).promise();
+
+    console.log("Get Group: ", result);
+    return !!result.Item;
+}
+
+async function getImagesPerGroup(groupId: string) {
+
+    const result = await docClient.query({
+        TableName: imagesTable,
+        KeyConditionExpression: "groupId = :groupId",
+        ExpressionAttributeValues: {
+            ":groupId": groupId
+        },
+        ScanIndexForward: false
+    }).promise();
+
+    return result.Items;
+}
