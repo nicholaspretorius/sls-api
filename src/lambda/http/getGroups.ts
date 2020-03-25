ec83b0b0e8f7b7afab025a0182a7b1a5404a2d7e
Nicholas Pretorius <nicholaspretorius@gmail.com>
2020-03-25 10:24:41 +0200

    feat: Refactor get and create group

--- src/lambda/http/getGroups.ts
+++ src/lambda/http/getGroups.ts
@@ -1,19 +1,11 @@
 import { APIGatewayProxyHandler, APIGatewayProxyEvent, APIGatewayProxyResult } from 'aws-lambda';
 import "source-map-support/register";
-import * as AWS from "aws-sdk";
-
-const docClient = new AWS.DynamoDB.DocumentClient();
-
-const groupsTable = process.env.GROUPS_TABLE;
+import { getAllGroups } from "./../../businessLogic/groups";
 
 export const handler: APIGatewayProxyHandler = async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
     console.log("Processing Event: ", event);
 
-    const result = await docClient.scan({
-        TableName: groupsTable
-    }).promise();
-
-    const items = result.Items;
+    const groups = getAllGroups();
 
     return {
         statusCode: 200,
@@ -21,8 +13,35 @@ export const handler: APIGatewayProxyHandler = async (event: APIGatewayProxyEven
             "Access-Control-Allow-Origin": "*",
             "Content-Type": "application/json"
         },
-        body: JSON.stringify({ items })
+        body: JSON.stringify({ items: groups })
     };
 }
+
+// import { APIGatewayProxyHandler, APIGatewayProxyEvent, APIGatewayProxyResult } from 'aws-lambda';
+// import "source-map-support/register";
+// import * as AWS from "aws-sdk";
+
+// const docClient = new AWS.DynamoDB.DocumentClient();
+
+// const groupsTable = process.env.GROUPS_TABLE;
+
+// export const handler: APIGatewayProxyHandler = async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
+//     console.log("Processing Event: ", event);
+
+//     const result = await docClient.scan({
+//         TableName: groupsTable
+//     }).promise();
+
+//     const items = result.Items;
+
+//     return {
+//         statusCode: 200,
+//         headers: {
+//             "Access-Control-Allow-Origin": "*",
+//             "Content-Type": "application/json"
+//         },
+//         body: JSON.stringify({ items })
+//     };
+// }
 
 
